b5f745b0f5f428a8ac3c42019a04192284cce3fd
ChociMilki <chocimilki@gmail.com>
2023-09-19 08:35:39 -0800

    npc follow player working - cleaned up heirarchy

--- Assets/Tracie/ScriptsMaster/Gameplay/NPCMovement.cs
+++ Assets/Tracie/ScriptsMaster/Gameplay/NPCMovement.cs
@@ -4,22 +4,36 @@ using UnityEngine;
 
 public class NPCMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Transform player;
+    [SerializeField] private float followSpeed;
+    [SerializeField] private float stoppingDistance;
+
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        NPCFollowPlayer(); 
+
     }
     /// <summary>
     ///  npc goes from static to players location and stops when they are x proximity from the player 
     /// </summary>
     private void NPCFollowPlayer()
     {
-
+        if (player == null) { return; }
+        // calc distance between npc and player 
+        float distanceToPlayer = Vector2.Distance(transform.position, player.position);
+        // check if npc is outside of stopping distance 
+        if (distanceToPlayer >= stoppingDistance)
+        {
+            //calc direction from npc to player 
+            Vector2 directionToPlayer = player.position - transform.position;
+            // normalize 
+            directionToPlayer.Normalize();
+            // move npc towards player at x speed 
+            transform.Translate(Vector2.left * followSpeed * Time.deltaTime * directionToPlayer); 
+        }
+        Debug.Log("Npc on the move"); 
     }
 }
