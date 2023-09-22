79411574c89704e05af4dc5a40bb5cb7892037e2
ChociMilki <chocimilki@gmail.com>
2023-09-21 16:11:13 -0800

    working on scene management

---
+++ Assets/Tracie/ScriptsMaster/ControlsPanel.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI; 
+/// <summary>
+///  tt: handles on click for controls panel 
+/// </summary>
+public class ControlsPanel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //  one method for setting panel disactive  on click 
+        //one for setting active on click
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
